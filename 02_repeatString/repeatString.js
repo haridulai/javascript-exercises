4feed6a7228c6826f4e0bc62951f5ca14b9d3205
Hari Dulai <hdcodesdev@gmail.com>
2023-05-14 18:24:31 +0100

    02 repeatString test success

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function (text, iteration) {
+  if (iteration < 0) {
+    return "ERROR";
+  }
 
+  let message = "";
+
+  for (let i = 0; i < iteration; i++) {
+    message += text;
+  }
+
+  return message;
 };
 
 // Do not edit below this line
